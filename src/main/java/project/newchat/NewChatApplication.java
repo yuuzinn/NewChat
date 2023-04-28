7b1ebc2c4e13885c7bdbbdbff4f90e638016093e
yeb0 <s110528@nate.com>
2023-04-28 16:18:12 +0900

    ...first commit

---
+++ src/main/java/project/newchat/NewChatApplication.java
@@ -0,0 +1,13 @@
+package project.newchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/newchat/NewChatApplicationTests.java
@@ -0,0 +1,13 @@
+package project.newchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
