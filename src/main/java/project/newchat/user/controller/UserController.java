8c831ab380b34462a355bd188f38fa63ecfcc655
yeb0 <s110528@nate.com>
2023-05-03 15:44:40 +0900

    Refactor : 리뷰 반영 회원가입 유효성(valid)추가, @RequestPart -> Body 변경

--- src/main/java/project/newchat/user/controller/UserController.java
+++ src/main/java/project/newchat/user/controller/UserController.java
@@ -2,11 +2,15 @@ package project.newchat.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import project.newchat.user.domain.request.UserRequest;
 import project.newchat.user.service.UserService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,8 +19,8 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/user")
-    public ResponseEntity<Object> signUp (
-            @RequestPart(value = "data")UserRequest userRequest) {
+    public ResponseEntity<Object> signUp(
+            @RequestBody @Valid UserRequest userRequest) {
         userService.signUp(userRequest);
         return ResponseEntity.ok().body("success");
     }
