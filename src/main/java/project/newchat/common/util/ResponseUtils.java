d96e506c4b3b429f0127cfb49569351d533115d7
yeb0 <s110528@nate.com>
2023-05-17 01:27:53 +0900

    refactor : 리뷰 반영 ResponseUtils 메시지String -> enum 처리

--- src/main/java/project/newchat/common/util/ResponseUtils.java
+++ src/main/java/project/newchat/common/util/ResponseUtils.java
@@ -1,13 +1,13 @@
 package project.newchat.common.util;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import project.newchat.common.type.ResponseMessage;
 
 public class ResponseUtils {
-  public static ResponseEntity<Object> ok(String message, Object result) {
+  public static ResponseEntity<Object> ok(ResponseMessage message, Object result) {
     Map<String, Object> response = new LinkedHashMap<>();
     response.put("isSuccess", true);
     response.put("code", HttpStatus.OK.value());
@@ -16,7 +16,7 @@ public class ResponseUtils {
     return ResponseEntity.ok(response);
   }
 
-  public static ResponseEntity<Object> ok(String message) {
+  public static ResponseEntity<Object> ok(ResponseMessage message) {
     Map<String, Object> response = new LinkedHashMap<>();
     response.put("isSuccess", true);
     response.put("code", HttpStatus.OK.value());
@@ -24,28 +24,11 @@ public class ResponseUtils {
     return ResponseEntity.ok(response);
   }
 
-  public static ResponseEntity<Object> notFound(String message) {
+  public static ResponseEntity<Object> notFound(ResponseMessage message) {
     Map<String, Object> response = new LinkedHashMap<>();
     response.put("isSuccess", false);
     response.put("code", HttpStatus.NOT_FOUND.value());
     response.put("message", message);
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
-  }
-
-
-  public static ResponseEntity<Object> badRequest(String message) {
-    Map<String, Object> response = new LinkedHashMap<>();
-    response.put("isSuccess", false);
-    response.put("code", HttpStatus.BAD_REQUEST.value());
-    response.put("message", message);
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-  }
-
-  public static ResponseEntity<Object> serverError(String message) {
-    Map<String, Object> response = new LinkedHashMap<>();
-    response.put("isSuccess", false);
-    response.put("code", HttpStatus.INTERNAL_SERVER_ERROR.value());
-    response.put("message", message);
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
   }
 }
