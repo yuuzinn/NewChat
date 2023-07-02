552fb4d826cab26165b52511a1aa713c8a6b37a8
yeb0 <s110528@nate.com>
2023-07-03 07:53:49 +0900

    refactor : Service 단에 API 설명 주석 추가

--- src/main/java/project/newchat/friend/service/FriendService.java
+++ src/main/java/project/newchat/friend/service/FriendService.java
@@ -6,13 +6,28 @@ import project.newchat.friend.dto.FriendDto;
 
 public interface FriendService {
 
+  /**
+   * 친구 추가
+   */
   void addFriend(Long fromUserId, Long toUserId);
 
+  /**
+   * 친구 요청 수락
+   */
   void receive(Long fromUserId, Long toUserId);
 
+  /**
+   * 친구 요청 거절
+   */
   void refuse(Long fromUserId, Long toUserId);
 
+  /**
+   * 친구 삭제
+   */
   void delete(Long fromUserId, Long toUserId);
 
+  /**
+   * 친구 목록 조회
+   */
   List<FriendDto> selectFriendList(Long fromUserId, Pageable pageable);
 }
